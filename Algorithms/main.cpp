b4b0f172471a6a027bb2c411977f0a656b6327d1
dennisyakovlev <dennis.yakovlev@yahoo.ca>
2020-12-28 12:12:44 -0500

    added linear search

--- Algorithms/main.cpp
+++ Algorithms/main.cpp
@@ -1,17 +1,10 @@
 #include <iostream>
-#include <vector>
-#include <string>
-#include "Headers/Search Algorithms/Linear Search.h"
-#include "Header.h"
-#include <algorithm>
-#include <xutility>
+
 
 using namespace std;
 
 int main() {
 
-	vector<Test> v{ Test(1),Test(2),Test(3),Test(4) };
-
-	auto mhm = algorithms::search::Linear_Search(v.begin(), v.end(), false);
+	cout << "print" << endl;
 
 }
